a8c85d28f36b9bc6501797aaa70057852d16b511
canneron <mcgreevycameron@gmail.com>
2024-06-23 22:16:59 +0100

    P2P communication not working, adding to fix

---
+++ filetransfer.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+type Node struct {
+	hostname string
+	port     int
+}
+
+func connectionServer(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Connection closed")
+			return
+		}
+
+		fmt.Print("Received:", message)
+		conn.Write([]byte("Message Received\n"))
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <hostname> <port>")
+		return
+	}
+
+	hostname := os.Args[1]
+	port := os.Args[2]
+
+	go func() {
+		portLN := ":" + port
+		listener, err := net.Listen("tcp", portLN)
+		if err != nil {
+			fmt.Println("Error starting TCP:", err)
+			os.Exit(1)
+		}
+
+		defer listener.Close()
+
+		fmt.Println("Listening on", hostname)
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection:", err)
+				continue
+			}
+
+			go connectionServer(conn)
+		}
+	}()
+
+	go func() {
+		reader1 := bufio.NewReader(os.Stdin)
+		fmt.Println("Enter message: ")
+		portConnect, _ := reader1.ReadString('\n')
+		fmt.Scanln(&portConnect)
+		serverInfo := fmt.Sprintf("%s:%s", hostname, portConnect)
+		server, err := net.Dial("tcp", serverInfo)
+
+		if err != nil {
+			fmt.Println("Error connecting to server:", err)
+			os.Exit(1)
+		}
+
+		defer server.Close()
+
+		fmt.Println("Connected to server")
+
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Println("Enter message: ")
+			text, _ := reader.ReadString('\n')
+			fmt.Fprintf(server, text)
+
+			message, _ := bufio.NewReader(server).ReadString('\n')
+			fmt.Println("Response:", message)
+		}
+	}()
+}
